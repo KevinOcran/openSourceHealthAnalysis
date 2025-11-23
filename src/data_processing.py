fdb434b9414663279f89be3f5c94e9d08f634f7e
Kevin Ampong Ocran <kevinocran2@gmail.com>
2025-11-23 19:39:26 +0000

    cleaned notebooks

--- src/data_processing.py
+++ src/data_processing.py
@@ -180,6 +180,140 @@ def extract_info(repo_data):
     return pd.DataFrame(all_info)
 
 
+def compute_issues_summary(issues_df):
+    # Group issue data by repo_name and perform summary stats:
+    issues_summary = issues_df.groupby('repo_name').agg({
+        'issue_id': 'count',
+        'issue_age_days': ['mean', 'median']
+    }).reset_index()
+
+    issues_summary.columns = [
+        f'{top_level}_{bottom_level}' for top_level, bottom_level in issues_summary.columns]
+    issues_summary = issues_summary.rename(columns={
+        'repo_name_': 'repo_name',
+        'issue_id_count': 'number_of_issues',
+        'issue_age_days_mean': 'avg_issue_age_days',
+        'issue_age_days_median': 'median_issue_age_days'
+    })
+
+    return issues_summary
+
+
+def compute_commits_summary(commits_df):
+    # Group commit data by repo_name and perform aggregation:
+    commits_summary = commits_df.groupby('repo_name').agg({
+        'sha': 'count'
+    }).reset_index().rename(columns={'sha': 'commit_count', 'repo_name': 'repo_name'})
+
+    return commits_summary
+
+
+def compute_info_summary(info_df):
+    # Group info data by repo_name and perform aggregation:
+    info_summary = info_df.groupby('repo_name').agg({
+        'stars': 'sum'
+    }).reset_index().rename(columns={'stars': 'stargazers_sum'})
+
+    return info_summary
+
+
+def compute_closed_issues_summary(issues_df):
+    # Create new column, closed_issues to hold the time it took to close issues (only actually closed issues):
+    closed_issues = issues_df[['repo_name', 'time_to_close_hours']].dropna(subset=[
+                                                                           'time_to_close_hours'])
+
+    # Group time to close issues by 'repo_name' and calculate 'average_time_to_close issues' for each repo:
+    closed_issues_summary = closed_issues.groupby('repo_name').agg({
+        'time_to_close_hours': 'mean',
+    }).reset_index().rename(columns={'time_to_close_hours': 'average_time_to_close_hours'})
+
+    return closed_issues_summary
+
+
+def compute_resolve_time_comparison(issues_df):
+    # Create resolve_time_cmpr to hold the time it took to close issues and the author association of closed issues:
+    resolve_time_cmpr = issues_df[['repo_name', 'time_to_close_hours',
+                                   'author_association']].dropna(subset=['time_to_close_hours'])
+
+    # Group resolve_time_cmpr by repo_name and author_association and then find the mean and median time for each author_association and repo:
+    resolve_time_cmpr = resolve_time_cmpr.groupby(['repo_name', 'author_association']).agg({
+        'time_to_close_hours': ['mean', 'median']
+    })
+
+    # Flatten new columns:
+    resolve_time_cmpr.columns = [
+        f'{top_level}_{bottom_level}' for top_level, bottom_level in resolve_time_cmpr.columns]
+
+    resolve_time_cmpr = resolve_time_cmpr.rename(columns={
+        'time_to_close_hours_mean': 'avg_time_to_close',
+        'time_to_close_hours_median': 'median_time_to_close'
+    }).reset_index()
+
+    return resolve_time_cmpr
+
+
+def compute_commits_by_date(commits_df):
+    # Create commits_by_date to store repo_name, author_date and message_length:
+    commits_by_date = commits_df[['repo_name',
+                                  'author_date', 'message_length']].copy()
+
+    commits_by_date['day_of_the_week'] = commits_by_date['author_date'].dt.day_name()
+    commits_by_date['author_date'] = commits_by_date['author_date'].dt.date
+
+    return commits_by_date
+
+
+def compute_commits_per_day(commits_by_date):
+    # Create commits_per_day_df to hold the count of the number of commits per day by repo_name:
+    commits_per_day = commits_by_date.groupby(
+        ['repo_name', 'author_date']).size()
+    commits_per_day_df = commits_per_day.reset_index(name='number_of_commits')
+    commits_per_day_df = commits_per_day_df.set_index(
+        ['repo_name', 'author_date'])
+
+    return commits_per_day_df
+
+
+def compute_day_analysis(commits_by_date):
+    # Which days had the most commits:
+    day_analysis = commits_by_date.groupby(['repo_name', 'day_of_the_week']).agg({
+        'day_of_the_week': 'count'
+    }).rename(columns={'day_of_the_week': 'number_of_commits_on_day'}).reset_index()
+
+    return day_analysis
+
+
+def compute_commit_msg_analysis(commits_by_date):
+    # Message Length analysis:
+    commit_msg_analysis = commits_by_date.copy()
+    commit_msg_analysis = commit_msg_analysis.groupby(['repo_name']).agg({
+        'message_length': ['mean', 'median']
+    })
+
+    # Flatten new columns:
+    commit_msg_analysis.columns = [
+        f'{top_level}_{bottom_level}' for top_level, bottom_level in commit_msg_analysis.columns]
+
+    commit_msg_analysis = commit_msg_analysis.rename(columns={
+        'message_length_mean': 'avg_message_length',
+        'message_length_median': 'median_message_length'
+    }).reset_index()
+
+    return commit_msg_analysis
+
+
+def compute_final_summary(issues_summary, commits_summary, info_summary, closed_issues_summary):
+
+    summary_df = pd.merge(issues_summary, commits_summary,
+                          on='repo_name', how='inner')
+    summary_df = pd.merge(summary_df, info_summary,
+                          on='repo_name', how='inner')
+    summary_df = pd.merge(summary_df, closed_issues_summary,
+                          on='repo_name', how='inner')
+
+    return summary_df
+
+
 # define save methd:
 
 def save_data(dataframe, filename):
